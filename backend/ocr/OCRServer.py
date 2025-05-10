0cd11993ae2f787d507a1a47cbaeef21fea20126
Mohamed <mohamed@deepscenario.com>
2025-05-10 10:49:13 +0200

    add data

---
+++ backend/ocr/OCRServer.py
@@ -0,0 +1,37 @@
+import base64
+
+from mistralai import Mistral
+
+def encode_image(image_path):
+    """Encode the image to base64."""
+    try:
+        with open(image_path, "rb") as image_file:
+            return base64.b64encode(image_file.read()).decode('utf-8')
+    except FileNotFoundError:
+        print(f"Error: The file {image_path} was not found.")
+        return None
+    except Exception as e:  # Added general exception handling
+        print(f"Error: {e}")
+        return None
+
+
+API_KEY_PATH = '../../.venv/MISTRAL_API_KEY'
+api_key = open(API_KEY_PATH, 'r').read()
+
+client = Mistral(api_key=api_key)
+
+image_path = "path_to_your_image.jpg"
+
+# Getting the base64 string
+base64_image = encode_image(image_path)
+
+api_key = os.environ["MISTRAL_API_KEY"]
+client = Mistral(api_key=api_key)
+
+ocr_response = client.ocr.process(
+    model="mistral-ocr-latest",
+    document={
+        "type": "image_url",
+        "image_url": f"data:image/jpeg;base64,{base64_image}"
+    }
+)
